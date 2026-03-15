468ff792a4136e236e62c0adf077c932520557bd
shahd Alaa <shahdalaa3172003@gmail.com>
2026-03-15 14:46:04 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+#region Q1
+// Part 1 — Enums
+// using System;
+
+// enum DayOfWeekEnum
+// {
+//     Saturday,
+//     Sunday,
+//     Monday,
+//     Tuesday,
+//     Wednesday,
+//     Thursday,
+//     Friday
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Enter a day number (0-6): ");
+//         int num = int.Parse(Console.ReadLine());
+
+//         DayOfWeekEnum day = (DayOfWeekEnum)num;
+
+//         Console.WriteLine("Day: " + day);
+
+//         switch (day)
+//         {
+//             case DayOfWeekEnum.Saturday:
+//             case DayOfWeekEnum.Friday:
+//                 Console.WriteLine("It's the Weekend");
+//                 break;
+
+//             default:
+//                 Console.WriteLine("It's a Workday");
+//                 break;
+//         }
+//     }
+// }
+ #endregion
+
+
+
+
+// #endregion
