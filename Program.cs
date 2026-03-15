a93758c0c5dbe3e0cdaf3da99d7e8020b7a05e55
shahd Alaa <shahdalaa3172003@gmail.com>
2026-03-15 14:47:43 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -38,8 +38,85 @@
 //     }
 // }
  #endregion
+#region Q2
+// Part 2 — Arrays (Array Statistics)
 
+// using System;
 
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Enter array size: ");
+//         int size = int.Parse(Console.ReadLine());
 
+//         int[] arr = new int[size];
 
-// #endregion
+//         for (int i = 0; i < size; i++)
+//         {
+//             Console.Write($"Enter element [{i}]: ");
+//             arr[i] = int.Parse(Console.ReadLine());
+//         }
+
+//         int sum = 0;
+//         int max = arr[0];
+//         int min = arr[0];
+
+//         for (int i = 0; i < size; i++)
+//         {
+//             sum += arr[i];
+
+//             if (arr[i] > max)
+//                 max = arr[i];
+
+//             if (arr[i] < min)
+//                 min = arr[i];
+//         }
+
+//         double avg = (double)sum / size;
+
+//         Console.WriteLine("Sum = " + sum);
+//         Console.WriteLine("Average = " + avg);
+//         Console.WriteLine("Max = " + max);
+//         Console.WriteLine("Min = " + min);
+
+//         Console.Write("Reverse = ");
+
+//         for (int i = size - 1; i >= 0; i--)
+//             Console.Write(arr[i] + " ");
+//     }
+// }
+// Part 2 — Q2 (Matrix)
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         int[,] grades = new int[3, 4];
+//         double total = 0;
+
+//         for (int i = 0; i < 3; i++)
+//         {
+//             int sum = 0;
+
+//             Console.WriteLine($"Student {i + 1}");
+
+//             for (int j = 0; j < 4; j++)
+//             {
+//                 Console.Write($"Enter grade {j + 1}: ");
+//                 grades[i, j] = int.Parse(Console.ReadLine());
+//                 sum += grades[i, j];
+//             }
+
+//             double avg = sum / 4.0;
+//             total += avg;
+
+//             Console.WriteLine("Average = " + avg);
+//         }
+
+//         Console.WriteLine("Class Average = " + total / 3);
+//     }
+// }
+ #endregion
